c2c6cd15c6b08e2f70b10c3a6ec886a7a2d292d0
user <user@this.com>
2022-08-17 13:29:36 -0400

    Added gradle project

---
+++ mcc-authentication-service/src/main/java/com/bah/msd/mcc/MccAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bah.msd.mcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MccAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MccAuthenticationServiceApplication.class, args);
+	}
+
+}
