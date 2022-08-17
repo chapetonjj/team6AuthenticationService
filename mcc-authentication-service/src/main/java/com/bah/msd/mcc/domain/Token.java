4d8c04059d2401b6b72e64575f9a0882a94e1bf6
user <user@this.com>
2022-08-17 13:56:28 -0400

    "Added packages and token files"

---
+++ mcc-authentication-service/src/main/java/com/bah/msd/mcc/domain/Token.java
@@ -0,0 +1,18 @@
+package com.bah.msd.mcc.domain;
+
+public class Token {
+	String token;
+
+	public Token(String token) {
+		super();
+		this.token = token;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+}
