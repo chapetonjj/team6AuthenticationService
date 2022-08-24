09169c4f34ca08c175ce99c0289f2b15054c8c96
user <user@this.com>
2022-08-24 11:53:08 -0400

    Clone from git, removed old java files.

--- JWTUtil.java
+++
@@ -1,7 +0,0 @@
-package com.example.api;
-
-public interface JWTUtil {
-	public boolean verifyToken(String jwt_token);
-	public String getScopes(String jwt_token) ;
-	public Token createToken(String scopes) ;
-}
